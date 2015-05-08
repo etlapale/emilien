28c9db1b79402e6b9828b715f53afa7affa97902
Émilien Tlapale <emilien@tlapale.com>
2015-05-08 14:07:52 -0700

    Changing copyright.

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Émilien Tlapale Homepage'
-copyright = u'2015, Émilien Tlapale'
+copyright = u'2012–2015 Émilien Tlapale'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
