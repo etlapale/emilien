18b3ff1c85b9e2a8413acb3370f8cb06fb8c81af
Émilien Tlapale <emilien@tlapale.com>
2015-05-08 13:28:15 -0700

    Extending poco-sphinx.

--- source/conf.py
+++ source/conf.py
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'poco-sphinx'
+html_theme = 'emilien-sphinx'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
