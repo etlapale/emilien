1ea2ff3dd8efc91ebfd52555ec927aba245d33d3
Émilien Tlapale <emilien@tlapale.com>
2015-05-10 21:00:33 -0700

    Fixing website title.

--- source/conf.py
+++ source/conf.py
@@ -112,7 +112,7 @@ html_context = {'source_repository': 'https://git.atelo.org/etlapale/homepage/bl
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = u'Émilien Tlapale'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
